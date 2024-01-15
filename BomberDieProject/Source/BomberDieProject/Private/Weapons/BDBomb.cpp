b689059e7e95349881b65d7a9ad6c1ae7b29b4fd
Arcreuss <63006702+Arcreuss@users.noreply.github.com>
2024-01-14 21:50:12 -0500

    [ADD] Bomb.cpp/h + Collision Bomb preset + BP_Test explosion

---
+++ BomberDieProject/Source/BomberDieProject/Private/Weapons/BDBomb.cpp
@@ -0,0 +1,90 @@
+﻿// UnrealEngine Starter template - by TechNet 2023
+
+
+#include "Weapons/BDBomb.h"
+
+#include "Components/SphereComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+
+// Sets default values
+ABDBomb::ABDBomb()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+	TimerHandle.Invalidate();
+
+	BombMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BombMesh"));
+	RootComponent = BombMesh;
+
+	BoxCollision = CreateDefaultSubobject<USphereComponent>(TEXT("BoxCollision"));
+	BoxCollision->SetupAttachment(RootComponent);
+}
+
+// Called when the game starts or when spawned
+void ABDBomb::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if(!HasAuthority()) return;
+
+	const FTimerDelegate TimerDelegate = FTimerDelegate::CreateUObject(this, &ABDBomb::Explode, Distance);
+	GetWorld()->GetTimerManager().SetTimer(TimerHandle, TimerDelegate, MyDelay, false);
+}
+
+void ABDBomb::Explode(int ExplosionLength)	// This function is called by the TimerHandle
+{
+	GetWorld()->GetTimerManager().ClearTimer(TimerHandle);
+	DisplayBombFX();
+
+	bIsExploded = true;
+	ExplosionLength = ExplosionLength * DISTANCE_UNIT;
+	TArray<FHitResult> Results;
+
+	Results.Append(BombTraceChannel(true, ExplosionLength));
+	Results.Append(BombTraceChannel(true, -ExplosionLength));
+	Results.Append(BombTraceChannel(false, ExplosionLength));
+	Results.Append(BombTraceChannel(false, -ExplosionLength));
+
+	if (!Results.IsEmpty())
+	{
+		for (auto& Hit : Results)
+		{
+			UGameplayStatics::ApplyDamage(Hit.GetActor(), Damage, (GetInstigator() ? GetInstigator()->GetController() : nullptr), this, DamageType);
+		}
+	}
+
+
+	DrawDebugLine(GetWorld(), GetActorLocation(), GetActorLocation() + FVector(ExplosionLength, 0, 0), FColor::Red, false, 5.f, 0, 1.f);
+	DrawDebugLine(GetWorld(), GetActorLocation(), GetActorLocation() + FVector(0, ExplosionLength, 0), FColor::Red, false, 5.f, 0, 1.f);
+	DrawDebugLine(GetWorld(), GetActorLocation(), GetActorLocation() + FVector(- ExplosionLength, 0, 0), FColor::Red, false, 5.f, 0, 1.f);
+	DrawDebugLine(GetWorld(), GetActorLocation(), GetActorLocation() + FVector(0, - ExplosionLength, 0), FColor::Red, false, 5.f, 0, 1.f);
+
+	GEngine->AddOnScreenDebugMessage( -1, 5.f, FColor::Red, TEXT("Explode !"));
+	UE_LOG(LogTemp, Warning, TEXT("UNIT is : %d !"), ExplosionLength);
+}
+
+TArray<FHitResult> ABDBomb::BombTraceChannel(bool AxeX, int ExplosionLength)
+{
+	TArray<FHitResult> Results;
+
+	if (GetWorld()->SweepMultiByChannel(
+		OutHitResult,
+		GetActorLocation(),
+		GetActorLocation() + (AxeX ? FVector(ExplosionLength, 0, 0) : FVector(0, ExplosionLength, 0)),
+		FQuat::Identity,
+		BombCollisionChannel,
+		FCollisionShape::MakeSphere(DISTANCE_UNIT / 2),
+		FCollisionQueryParams::DefaultQueryParam,
+		FCollisionResponseParams::DefaultResponseParam))
+	{
+		Results = OutHitResult;
+	}
+	return Results;
+}
+
+void ABDBomb::DisplayBombFX_Implementation() const
+{
+	//To be continued...
+}
+
---
+++ BomberDieProject/Source/BomberDieProject/Public/Weapons/BDBomb.h
@@ -0,0 +1,66 @@
+﻿// UnrealEngine Starter template - by TechNet 2023
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BDBomb.generated.h"
+
+#define DISTANCE_UNIT 100
+
+UCLASS()
+class BOMBERDIEPROJECT_API ABDBomb : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ABDBomb();
+
+	UFUNCTION(BlueprintCallable, Category = "Bomb")
+	void Explode(int Distance);
+
+	UFUNCTION()
+	TArray<FHitResult> BombTraceChannel(bool AxeX, int ExplosionLength);
+
+	UFUNCTION(NetMulticast, Reliable, Category = "Bomb") // This function is called by the server and played on all clients, Reliable is like UDP
+	void DisplayBombFX() const;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Bomb")
+	class UStaticMeshComponent* BombMesh;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Bomb")
+	class USphereComponent* BoxCollision;
+
+	UPROPERTY(BlueprintReadOnly)
+	bool bIsExploded = false;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Bomb")
+	int Distance = 3;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Bomb")
+	float MyDelay = 3.f;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Bomb")
+	float Damage = 100.f;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Bomb")
+	TSubclassOf<UDamageType> DamageType;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Bomb")
+	TEnumAsByte<ECollisionChannel> BombCollisionChannel;
+
+
+private:
+	UPROPERTY()
+	TArray<FHitResult> OutHitResult;
+
+	UPROPERTY()
+	FTimerHandle TimerHandle;
+};
